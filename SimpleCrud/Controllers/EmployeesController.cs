f5f535e1860b0e07cecd22cef63a976e874799bb
Antonio Henrique <anotniohenriquebv@gmail.com>
2024-05-26 00:14:28 -0300

    Create EmployeesController and Get method

---
+++ SimpleCrud/Controllers/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleCrud.DTOs;
+using SimpleCrud.DTOs.Mapping;
+using SimpleCrud.Repositories.Interfaces;
+
+namespace SimpleCrud.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(EmployeeDTO employeeDTO)
+        {
+            await _employeeRepository.Add(employeeDTO.ToEmployee());
+            return Ok();
+        }
+    }
+}
